a5914baf7302fe8614988a3bc4dd1b03b4dcf81a
dsong127 <dsong127@gmail.com>
2020-02-26 16:19:18 -0800

    server client communication with user input

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,52 @@ import React, { Component } from 'react';
 
 class App extends Component{
   state = {
+    responseFromServer: null,
     data: null,
     userid: null,
-    success: null
+    success: null,
   };
  
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => res.text())
-      .then(res => {
-        let resjson = JSON.parse(res);
-        console.log(resjson);
-        this.setState({ data: res});//, userid: resjson.response.steamid, success: resjson.response.success });
-      })
-      .catch(err => console.log(err));
-  };
+  // DO WE NEED THIS?
+  // componentDidMount() {
+  //     // Call our fetch function below once the component mounts
+  //   this.callBackendAPI()
+  //     .then(res => res.text())
+  //     .then(res => {
+  //       let resjson = JSON.parse(res);
+  //       // console.log(resjson);
+  //       this.setState({ data: res});//, userid: resjson.response.steamid, success: resjson.response.success });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
   
   // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('http://localhost:5000/search');
-    const body = await response;
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
+    const response = await fetch('http://localhost:5000/search', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userId: this.state.userid }),
+    });
+      const body = await response.text();
+      console.log(body);
+      // this.setState({ responseFromServer: body });
+      // console.log(this.responseFromServer);
   };
 
   render() {
     return (
       <div>
-        <h1>Hello World, React!</h1>
+        <h1>Steam ID</h1>
         <form>
           <label>Steam User ID: 
-            <input type="text" name="usersteamid" /> 
+            <input type="text" name="usersteamid" onChange={e => this.setState({ userid: e.target.value })}/> 
           </label>
-          <input type="submit" value="Submit" />
+          <input type="button" value="Submit" onClick={this.callBackendAPI}/>
         </form>
         <p>{this.state.data}</p>
-        {/* <p>{this.state.userid}</p>
-        <p>{this.state.success}</p> */}
+    
       </div>
     );
   }
--- server.js
+++ server.js
@@ -9,49 +9,46 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('steamconfig.json');
 let key = JSON.parse(rawdata).apiKey;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 let corsOptions = {};
 app.use(cors());
 
-app.get('/search', (req, res) => {
-    res.status(200);
-    res.set({
-        'Content-Type': 'text/html'
-    });
 
-    let usersteamname = "redunknown1"; //"dsongpdx";
 
-    let getSteamId = async () => {
-        let obj = await getJSON('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+ key +'&vanityurl=' + usersteamname);
-        console.log("obj", obj);
-        let usersteamid = obj.response.steamid;
-        console.log("usersteamid:", usersteamid);
-
-        let playerSum = await getJSON('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+ key +'&steamids=' + usersteamid);
-        console.log("playerSum:", playerSum);
-
-        let friends = await getJSON('http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key='+ key +'&steamid=' + usersteamid + "&relationship=friend");
-        console.log("friends:", friends);
-        let recentlyPlayed = await getJSON('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key='+ key +'&steamid=' + usersteamid);
-        console.log("recentlyPlayed:", recentlyPlayed);
-
-        let ownedGames = await getJSON('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+ key + '&steamid=' + usersteamid);
-        console.log("ownedGames:", ownedGames);
-
-        let gameslist = await getJSON('http://api.steampowered.com/ISteamApps/GetAppList/v2');
-        console.log("gameslist:", gameslist);
-
-        // res.write(usersteamid);
-        // res.write(JSON.stringify(playerSum));
-        // res.write(JSON.stringify(friends));
-        // res.write(JSON.stringify(recentlyPlayed));
-        // res.write(JSON.stringify(ownedGames));
-        // res.write(JSON.stringify(gameslist));
-        let data = [playerSum, friends, recentlyPlayed, ownedGames, gameslist];
-        res.write(JSON.stringify(data));
-        res.end();
-
-    }
-    getSteamId();
+app.post('/search', (req, res) => {
+    let steamName = req.body.userId;
+    
+    getSteamId(steamName, res);
 });
+
+
+async function getSteamId(usersteamname, response) {
+    let obj = await getJSON('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+ key +'&vanityurl=' + usersteamname)
+    console.log("obj", obj);
+    let usersteamid = obj.response.steamid;
+    console.log("usersteamid:", usersteamid);
+
+    let playerSum = await getJSON('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+ key +'&steamids=' + usersteamid);
+    console.log("playerSum:", playerSum);
+
+    let friends = await getJSON('http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key='+ key +'&steamid=' + usersteamid + "&relationship=friend");
+    console.log("friends:", friends);
+    let recentlyPlayed = await getJSON('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key='+ key +'&steamid=' + usersteamid);
+    console.log("recentlyPlayed:", recentlyPlayed);
+
+    let ownedGames = await getJSON('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+ key + '&steamid=' + usersteamid);
+    console.log("ownedGames:", ownedGames);
+
+    let gameslist = await getJSON('http://api.steampowered.com/ISteamApps/GetAppList/v2');
+    console.log("gameslist:", gameslist);
+
+    let data = [playerSum, friends, recentlyPlayed, ownedGames, gameslist];
+    response.send(data);
+
+}
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
