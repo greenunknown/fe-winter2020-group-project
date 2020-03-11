f9761359ca1283ae13775131b54955b6279d1f0e
dsong127 <dsong127@gmail.com>
2020-03-11 01:11:26 -0700

    Using Testing Library instead of Enzyme

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,32 @@
 import React from 'react';
-import {App} from './App';
+import App from './App';
 
-test('empty submission', () => {
+import {render, fireEvent, screen} from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect";
+
+it('renders without crashing', () => {
+    const {getByText} = render(<App />)
     
-});
+    expect(getByText('Steam Dash')).toBeInTheDocument()
+  })
+
+
+// it('test test', () => {
+//     const wrap = shallow(<UserProfile />);
+//     var playerSummary = {
+//         steamid: "testId",
+//         personaname: "testName",
+//         profileurl: "testURL",
+//         avatar: null,
+//         avatarmedium: null,
+//         avatarfull: 'process.env.PUBLIC_URL/testImage.png',
+//         lastlogoff: '1583907455',
+//         timecreated: '1583907455'
+//     }
+
+//     wrap.setProps(playerSummary)
+
+//     const personName = wrap.find('div.card-title').text();
+
+//     expect(personName).toBe('testName');
+// });
